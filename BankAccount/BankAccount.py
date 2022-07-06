b2cf08408226d96a2af68f9d67fe1ebf6f5747bb
Arwa-Alotaibi <arwa.alosaimi@outlook.sa>
2022-07-06 21:08:32 +0300

    init

--- BankAccount/BankAccount.py
+++ BankAccount/BankAccount.py
@@ -30,4 +30,15 @@ class BankAccount:
 
             print('the balance is negative')
 
-                
+
+    
+Arwa=BankAccount(0.01,77)
+Eman = BankAccount(0.01,100)
+#To the first account, make 3 deposits and 1 withdrawal, 
+# then yield interest and display the account's info all in one line of code (i.e. chaining)
+print('Arwa')
+Arwa.deposit(30).deposit(50).deposit(60).withdraw(20).yield_interest().display_account_info()
+print('Eman')
+#To the second account, make 2 deposits and 4 withdrawals, 
+# then yield interest and display the account's info all in one line of code (i.e. chaining)
+Eman.deposit(30).deposit(40).withdraw(20).withdraw(10).withdraw(5).withdraw(4).yield_interest().display_account_info()
